0ea9e4c5b266e3958f6d1dadfcc4b87e55ad01e9
derrickgenesism <133716280+derrickgenesism@users.noreply.github.com>
2026-08-12 12:08:57 +0300

    feat: add NextTopLoader for page transitions

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import MobileBottomNav from "@/components/MobileBottomNav";
+import NextTopLoader from 'nextjs-toploader';
 
 const inter = Inter({
   variable: "--font-inter",
@@ -19,6 +20,17 @@ export default function RootLayout({ children }) {
       className={`${inter.variable} antialiased`}
     >
       <body>
+        <NextTopLoader 
+          color="#e50914"
+          initialPosition={0.08}
+          crawlSpeed={200}
+          height={3}
+          crawl={true}
+          showSpinner={false}
+          easing="ease"
+          speed={200}
+          shadow="0 0 10px #e50914,0 0 5px #e50914"
+        />
         {children}
         <MobileBottomNav />
       </body>
